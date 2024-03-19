bef93588df5a5569c0995f0266d102f715120888
Edu-Fortes <eduardofortes.ma@gmail.com>
2024-03-19 18:33:34 -0300

    refact(hero): change component <div> element to <section>

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,12 @@
 export default function Hero({ children }) {
   return (
-    <div className='hero'>
+    <section className='hero'>
       <h2 className='hero__title'>O que está acontecendo no mundo?</h2>
       <p className='hero__subtitle'>
         Encontre as últimas notícias sobre qualquer tema e salve elas em sua
         conta pessoal
       </p>
       {children}
-    </div>
+    </section>
   );
 }
